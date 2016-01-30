f25861aced390fe52fb68ea1430f9bed3c48ca57
alexey-savin <alexeyv.savin@gmail.com>
2016-01-30 19:41:14 +0300

    Урок 4. Первый запрос

--- GeekBrains.VKPlayer/GeekBrains.VKPlayer/MainPage.xaml.cs
+++ GeekBrains.VKPlayer/GeekBrains.VKPlayer/MainPage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using VK.WindowsPhone.SDK;
+using VK.WindowsPhone.SDK.API;
+using VK.WindowsPhone.SDK.API.Model;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Navigation;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу http://go.microsoft.com/fwlink/?LinkId=391641
@@ -21,6 +25,21 @@ namespace GeekBrains.VKPlayer
 
             VKSDK.Initialize("5116069");
             VKSDK.Authorize(_scopeList, false, false);
+        }
+
+        private void button_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            trackView.Items.Clear();
+            var requestPar = new VKRequestParameters("audio.search", "q", trackRequest.Text);
+            Action<VKBackendResult<VKList<VKAudio>>> callback = (result) => 
+            {
+                foreach (var track in result.Data.items)
+                {
+                    trackView.Items.Add($"{track.artist} - {track.title}");
+                }
+            };
+
+            VKRequest.Dispatch<VKList<VKAudio>>(requestPar, callback);
         }
     }
 }
